53a115b77cfe8426a67d12fe469e7fcc00acde6e
mads illemann <you@example.com>
2019-02-09 21:46:55 +0100

    Begivenhed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { BegivenhederComponent } from './begivenheder/begivenheder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent,
+    BegivenhederComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/begivenheder/begivenheder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-begivenheder',
+  templateUrl: './begivenheder.component.html',
+  styleUrls: ['./begivenheder.component.css']
+})
+export class BegivenhederComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
