aafb0a8f1259450958d8aa00692925e7e688890e
Mads Illemann <mads849h@edu.eal.dk>
2019-02-13 09:46:06 +0100

    UserLogin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,17 @@ import { UsersService } from '../users.service';
 
 export class LoginComponent implements OnInit {
 
-  username: string = 'Mads';
-  password: string = '0000';
+  username: string;
+  password: string;
 
   constructor(private service: UsersService) {  }
 
   ngOnInit() {
-    this.service.CheckUser(this.username, this.password).subscribe( response => {
+  }
+
+
+  Sumbit()
+  {  this.service.CheckUser(this.username, this.password).subscribe( response => {
       console.log(response);
     });
   }
