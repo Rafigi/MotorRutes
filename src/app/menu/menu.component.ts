000382c07406d24194d6d7d2aec08fd3f1e2bf40
Mads Illemann <mads849h@edu.eal.dk>
2019-02-16 18:47:11 +0100

    Logud knap

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, RoutesRecognized} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routes: Router) { }
 
   ngOnInit() {
   }
 
+  Logud()
+  {
+    localStorage.removeItem('LoggedIn'); 
+    location.reload();
+    this.routes.navigate(['/login']);
+  }
 }
