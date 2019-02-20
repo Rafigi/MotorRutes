864d80c58c8c4bca93bf44b181c1eb008ad5d82c
Mads Illemann <mads849h@edu.eal.dk>
2019-02-20 15:14:33 +0100

    Maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { GooglePlaceDirective } from 'ngx-google-places-autocomplete/ngx-google-places-autocomplete.directive';
 import { ViewChild, ElementRef, NgZone, } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
 
 
 @Component({
@@ -15,6 +16,7 @@ public lat: Number = 0;
 public lng: Number = 0;
 public lat2: Number = 0;
 public lng2: Number = 0;
+
  
 public origin: any;
 public destination: any;
